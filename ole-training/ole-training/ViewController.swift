95fc3b7d9166476f69b21a95b96ce10835d7dfe0
Andre Sanches Bocato <andre@andre-bocato.local>
2019-01-28 14:11:12 -0200

    commit inicial

---
+++ ole-training/ole-training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ole-training
+//
+//  Created by Andre Sanches Bocato on 28/01/19.
+//  Copyright © 2019 Andre Sanches Bocato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
